46157071811f00f470b93f22d30f10b5b5c289d1
Danang Wijaya <danangwijaya750@gmail.com>
2021-03-03 09:00:27 -0800

    Initial Commit

---
+++ Dota 2 Heroes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dota 2 Heroes
+//
+//  Created by Danang Wijaya on 03/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Dota 2 Heroes")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dota 2 Heroes/Dota_2_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota_2_HeroesApp.swift
+//  Dota 2 Heroes
+//
+//  Created by Danang Wijaya on 03/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Dota_2_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
