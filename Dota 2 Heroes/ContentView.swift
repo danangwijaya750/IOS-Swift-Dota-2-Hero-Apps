1763c052d7d8a2e68023a8ea51d38cf05397f02e
Danang Wijaya <danangwijaya750@gmail.com>
2021-03-03 10:07:22 -0800

    Add Hero Indetifiable, and List of Heroes

--- Dota 2 Heroes/ContentView.swift
+++ Dota 2 Heroes/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Dota 2 Heroes")
-            .padding()
-    }
+struct Hero: Identifiable {
+    let id = UUID()
+    let heroName: String
+    let heroRole: String
+    let heroType: String
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct RowHero: View {
+    var hero: Hero
+    var body: some View {
+        Text("Name : \(hero.heroName)")
+        Text("Type : \(hero.heroType)")
+        Text("Role : \(hero.heroRole)")
     }
+}
+struct ContentView: View {
+    let heroes = [
+        Hero(heroName: "Anti Mage", heroRole: "Carry", heroType: "Agility"),
+        Hero(heroName: "Axe", heroRole: "Disabler", heroType: "Strength"),
+        Hero(heroName: "Ancient Aparation", heroRole:"Support", heroType: "Intelegence")
+    ]
+        var body: some View {
+            List(heroes){ hero in
+                RowHero(hero: hero)
+        }
+}
 }
